3f04240229612bae4f666ff4fb0d3b009ca73be8
SIKU-KR <peter012677@naver.com>
2025-04-09 17:36:29 +0900

    chapter 4 src cont.

--- src/chapter04/DiscountCondition.java
+++ src/chapter04/DiscountCondition.java
@@ -13,44 +13,25 @@ public class DiscountCondition {
     private LocalTime startTime;
     private LocalTime endTime;
 
+    public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
+        if (type != DiscountConditionType.PERIOD) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.dayOfWeek.equals(dayOfWeek) &&
+            this.startTime.isBefore(time) &&
+            this.endTime.isAfter(time);
+    }   
+
+    public boolean isDiscountable(int sequence) {
+        if (type != DiscountConditionType.SEQUENCE) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.sequence == sequence;
+    }
+
     public DiscountConditionType getType() {
         return type;
     }
-
-    public void setType(DiscountConditionType type) {
-        this.type = type;
-    }
-
-    public int getSequence() {
-        return sequence;
-    }
-
-    public void setSequence(int sequence) {
-        this.sequence = sequence;
-    }
-
-    public DayOfWeek getDayOfWeek() {
-        return dayOfWeek;
-    }
-
-    public void setDayOfWeek(DayOfWeek dayOfWeek) {
-        this.dayOfWeek = dayOfWeek;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalTime endTime) {
-        this.endTime = endTime;
-    }
-
 }
